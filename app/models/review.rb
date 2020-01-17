913eee4ff4914e53b4af796f1680f0abd2006891
Lara Bjork <lara.m.bjork@gmail.com>
2020-01-17 10:36:34 -0800

    add test and passing code to validate max length of Review content_body

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ApplicationRecord
   validates :author, presence: true
   validates :content_body, presence: true
   validates :rating, presence: true
+  validates_length_of :content_body, maximum: 250
+  # validates_length_of :content_body, minimum: 5
 end
