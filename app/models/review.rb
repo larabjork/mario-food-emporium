2bf6b7d3968fa20cb1ef66f1010d3c2e9d83b239
Lara Bjork <lara.m.bjork@gmail.com>
2020-01-17 10:38:02 -0800

    add test and passing code to validate minimum length of Review content_body

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
   validates_length_of :content_body, maximum: 250
-  # validates_length_of :content_body, minimum: 5
+  validates_length_of :content_body, minimum: 5
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,6 +6,5 @@ describe Review do
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
   it { should validate_length_of(:content_body).is_at_most(250)}
-
-  # it { should validate_length_of (:content_body).is_at_least(5)}
+  it { should validate_length_of(:content_body).is_at_least(5)}
 end
