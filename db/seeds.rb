17169d28e932cad33178afd2419ea39a51831746
Lara Bjork <lara.m.bjork@gmail.com>
2020-01-19 14:38:48 -0800

    add code to seed 250 reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
-
-50.times do |index|
+ 50.times do |index|
   Product.create!(name: Faker::Coffee.blend_name, cost: Faker::Number.number(digits: 2), country_of_origin: Faker:: Coffee.origin)
 end
 
 p "Created #{Product.count} products"
+
+Review.destroy_all
+products = Product.all
+250.times do |index|
+  Review.create!(author: Faker::Movies::HitchhikersGuideToTheGalaxy.character, content_body: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote[0..249], rating: Faker::Number.between(from: 1, to: 5), product_id: products.sample.id)
+end
+
+p "Created #{Review.count} reviews"
