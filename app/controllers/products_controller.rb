cb1692239890e876499e58e1a2bac63795e31620
Lara Bjork <lara.m.bjork@gmail.com>
2020-01-20 15:25:04 -0800

    add new routing, controller, views that mostly work for three assigned scopes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,62 +1,82 @@
 class ProductsController < ApplicationController
 
   def index
-    if params[:format]
-      # @products = Product.localness
-      @products = Product.three_most_recent
-      # @products = Product.most_reviews
-    else
-      @products = Product.all
-      render :index
-    end
+    @products = Product.all
+    render :index
   end
 
-
-
-  def new
-    @product = Product.new
-    render :new
+  def localness
+    params[:format]
+    @products = Product.localness
+    render :local
   end
 
+   def three_most_recent
+     params[:format]
+     @products = Product.three_most_recent
+     render :most_rev
+   end
+   def most_reviews
+     params[:format]
+     @products = Product.most_reviews
+     render :recents
+   end
 
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      flash[:notice] = "Product successfully added!"
-      redirect_to products_path
-    else
+
+
+  # if params[:format]
+  #   @products = Product.localness
+  #   render :local
+  #   # elsif params[:format => "localness"]
+  #   #   @products = Product.three_most_recent
+  #   #   render :recents
+  #   #   @products = Product.most_reviews
+  #   els
+
+    def new
+      @product = Product.new
       render :new
     end
-  end
 
-  def edit
-    @product = Product.find(params[:id])
-    render :edit
-  end
 
-  def show
-    @product = Product.find(params[:id])
-    render :show
-  end
+    def create
+      @product = Product.new(product_params)
+      if @product.save
+        flash[:notice] = "Product successfully added!"
+        redirect_to products_path
+      else
+        render :new
+      end
+    end
 
-  def update
-    @product= Product.find(params[:id])
-    if @product.update(product_params)
-      redirect_to products_path
-    else
+    def edit
+      @product = Product.find(params[:id])
       render :edit
     end
-  end
 
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    flash[:notice] = "Product deleted!"
-    redirect_to products_path
-  end
+    def show
+      @product = Product.find(params[:id])
+      render :show
+    end
 
-  private
-  def product_params
-    params.require(:product).permit(:name, :cost, :country_of_origin)
+    def update
+      @product= Product.find(params[:id])
+      if @product.update(product_params)
+        redirect_to products_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      flash[:notice] = "Product deleted!"
+      redirect_to products_path
+    end
+
+    private
+    def product_params
+      params.require(:product).permit(:name, :cost, :country_of_origin)
+    end
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'products#index'
   resources :products do
     resources :reviews
   end
+  get '/products.localness', to: 'products#localness', as: 'local'
+  get '/products.three_most_recent', to: 'products#three_most_recent', as: 'recents'
+  get '/products.most_reviews', to: 'products#most_reviews', as: 'most_rev'
+  root to: 'products#index'
 end
